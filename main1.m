5ef7abdaf51ebaf92f8ec4fd33e355cb0a5cb292
TwilightArchonYz <61190137+TwilightArchonYz@users.noreply.github.com>
2022-03-26 10:25:19 +0800

    Uploaded a new version of the AFO algorithm
    The gradient estimation strategy in the AFO algorithm is inefficient and difficult to apply to high-dimensional optimization problems, and a new method is used here for replacement.
    Here the replacement is Gaussian perturbation with a perturbation step of x_c the average distance from x
    Perturbation step of x_c distance x_m is also a good choice, interested in their own experiments.
    x_new=x_c+Rn*(x_r1-x_r2)*D_m;
    where D_m is a perturbation step of x_c the average distance from x.

---
+++ main1.m
@@ -0,0 +1,119 @@
+%% 2022.3.25 帝企鹅算法测试函数
+% AFO on shifted classcial benchmark functions
+clc;
+clear;
+close all;
+warning off
+%%
+rng('default')
+%% 测试函数
+global option
+option.no=7; %选择测试函数
+option.F=['F',num2str(option.no)];
+[lb,ub,dim,fobj] = Get_Functions_details(option.F);
+option.lb=lb;
+option.ub=ub;
+option.dim=dim;
+if length(option.lb)==1
+    option.lb=ones(1,option.dim)*option.lb;
+    option.ub=ones(1,option.dim)*option.ub;
+end
+option.fobj0=fobj;
+option.fobj=@fitFCN_BX;
+option.showIter=0;
+
+%% 实验参数
+option.repeatNum=10; % Number of repetitions of the test
+option.numAgent=150;        % size of population
+option.maxIteration=200;     % maximum number of interation
+option.v_lb=-(option.ub-option.lb)/4;
+option.v_ub=(option.ub-option.lb)/4;
+option.w2=0.5; %weight of Moving strategy III
+option.w4=1;%weight of Moving strategy III
+option.w5=1;%weight of Moving strategy III
+option.pe=0.01; % rate to judge Premature convergence
+option.gap0=ceil(sqrt(option.maxIteration*2))+1;
+option.gapMin=5; % min gap
+option.dec=2;    % dec of gap
+option.L=10;     % Catastrophe
+data=[];
+str_legend=[{'AFO1'},{'AFO2'},{'AFO3'}];
+for ii=1:option.repeatNum
+    ii
+    rng(ii)
+    %% shift classcial benchmark functions
+    option.RateB1=0.6; %Bound of Shift in x space
+    option.RateB2=0.3; %Bound of Shift in x space
+    temp1=rand(1,option.dim).*(option.RateB1-option.RateB2).*option.ub+option.RateB2.*option.ub;
+    temp2=rand(1,option.dim).*(option.RateB1-option.RateB2).*option.lb+option.RateB2.*option.lb;
+    tempR=rand(1,option.dim);
+    if lb==0
+        option.XB=temp1;
+    elseif ub==0
+        option.XB=temp2;
+    else
+        option.XB=zeros(1,option.dim);
+        option.XB(tempR>0.5)=temp1(tempR>0.5);
+        option.XB(tempR<0.5)=temp2(tempR<0.5);
+    end
+    option.XB=-option.XB;
+    %% Initialize population individuals (common to control experiment algorithm)
+    x=ones(option.numAgent,option.dim);
+    y=ones(option.numAgent,1);
+    for i=1:option.numAgent
+        x(i,:)=rand(size(option.lb)).*(option.ub-option.lb)+option.lb;
+        y(i)=option.fobj(x(i,:),option,data);
+    end
+    %% Ê¹ÓÃËã·¨Çó½â
+    % Based on the same population, solve the selected benchmarks functions by using different algorithms
+    bestX=x;
+    rng(ii)
+    tic
+    [bestY(1,:),bestX(1,:),recording(1)]=AFO1(x,y,option,data);
+    tt(ii,1)=toc;
+    rng(ii)
+    tic
+    [bestY(2,:),bestX(2,:),recording(2)]=AFO2(x,y,option,data);
+    tt(ii,2)=toc;
+    rng(ii)
+    tic
+    [bestY(3,:),bestX(3,:),recording(3)]=AFO3(x,y,option,data);
+    tt(ii,3)=toc;
+    for i=1:length(recording)
+        recordingCruve{i}(ii,1:option.maxIteration)=recording(i).bestFit(1:option.maxIteration);
+        recordingY(i,ii)=bestY(i,:);
+    end
+end
+%% Cruve
+figure
+hold on
+for i=1:length(recording)
+    if i>=8
+        plot(mean(recordingCruve{i}),'LineWidth',2)
+    else
+        plot(mean(recordingCruve{i}),'--','LineWidth',2)
+    end
+end
+legend(str_legend)
+%% Cruve based log
+figure
+hold on
+for i=1:length(recording)
+    if i >=8
+        plot(log(mean(recordingCruve{i})),'LineWidth',2)
+    else
+        plot(log(mean(recordingCruve{i})),'-.','LineWidth',2)
+    end
+end
+legend(str_legend)
+%% »ã×Ü½á¹û Aggregate results
+meanY=mean(recordingY');
+stdY=std(recordingY');
+minY=min(recordingY');
+maxY=max(recordingY');
+maeY=mean(abs(recordingY'-meanY));
+runningT=mean(tt(2:end,:));
+%% »æÖÆº¯ÊýÍ¼Ïñ Plotting function images
+option.F=['F',num2str(option.no)];
+figure
+func_plot(option.F)
